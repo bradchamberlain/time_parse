4dfbbd2651ccec4ef8fdf7c2c209addcc79b0911
bradchamberlain <brad.chamberlain@octanner.com>
2018-03-20 09:14:19 -0600

    Some simple refactoring

--- timeparse.rb
+++ timeparse.rb
@@ -7,16 +7,25 @@ def as_time s
  Time.new s[2] + 2000, s[0], s[1], s[3], s[4]
 end
 
+def calculate_minutes start_time, end_time
+	(end_time - start_time)/60
+end
+
+def break_time val
+	val > 0 ? "(#{val.floor} minute break)" : ""
+end
+
+
 File.foreach("data.txt") do |line|
 	key = line.split(" ")[0]
 	DATES[key] = Array.new unless DATES[key]
 	DATES[key] << as_time(line.strip!)
 end 
 
-DATES.keys.each do |key|
-	time_split = (DATES[key].length == 4 ? (DATES[key][1] - DATES[key][2])/60 : 0 )
-	minutes = ((DATES[key].first - DATES[key].last) / 60) - time_split
+DATES.each do |k, value|
+	time_split = (value.length == 4 ? calculate_minutes(value[2], value[1]) : 0 )
+	minutes = calculate_minutes(value.last, value.first) - time_split
 	hours = (minutes/60).floor
 	adjusted_minutes = (minutes - (hours * 60)).to_i
-	puts "#{key} = #{hours} hours #{adjusted_minutes} minutes (#{time_split.floor} minute break)"
+	puts "#{k} = #{hours} hours #{adjusted_minutes} minutes #{break_time time_split}"
 end
